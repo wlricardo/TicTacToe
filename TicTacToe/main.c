87167873332422432ad2334748da87dc5c506769
wlricardo <wlricardo@gmail.com>
2020-05-28 11:44:30 -0300

    Starting project

---
+++ TicTacToe/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Function prototypes **/
+void show_board();
+int select_square(int, int, int);
+void check_for_winner();
+
+int main()
+{
+   printf("Hello world!\n");
+   return 0;
+}
+
+/** Functions implementations **/
+/* Function to show the game board */
+void show_board() {
+}
+
+/* Function to select a board's empty square */
+int select_square(int, int, int) {
+}
+
+/* Function to verify if theres is a winner */
+void check_for_winner() {
+}
